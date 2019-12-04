f37ea982ddb74c682f91cd656a951cd1c51610af
alle-zhinzher <sanches2000009@gmail.com>
2019-12-04 16:45:02 +0200

    Add React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+// Redux
 import { Provider } from 'react-redux';
 import store from '../store';
-
+// Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Components
 import TestRedux from './TestRedux';
+import Home from './Home';
+// Styles
 import './App.scss'
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <TestRedux />
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/t" component={TestRedux} />
+                    </Switch>
+                </Router>
             </Provider>
         )
     }
