4fddf349f3c71eea8e190c61f2f39ed081ec4b2d
ParishkuraOleg <parishkura.oleg@gmail.com>
2019-12-05 08:17:33 +0200

    socket fix

--- server/index.js
+++ server/index.js
@@ -3,11 +3,34 @@ const express = require('express');
 const config = require('config');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')();
+//const io = require('socket.io')(server);
 const roulette = require('./roulette/roulette')
 
 const serverConfig = config.get('server');
 // App initialization
+const Roulette = new roulette(io);
+io.on('connection', function(socket){
+    //console.log("THIS");
+    //console.log(this);
+    socket.on('makeBet',(msg)=>{
+        if(this.gameState=="endGame"){
+          //error
+        }
+        const {user,bet,betType} = msg;
+        //console.log(user);
+        //console.log(bet);
+        //console.log(betType);
+        Roulette.makeBet(user,betType,bet);
+        io.emit("makeBet",Roulette.bets);
+    });
+
+    socket.on("startGame", ()=>{Roulette.startGame()});
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+  });
+
 
 app.use(express.json())
 
@@ -20,3 +43,5 @@ app.use('/api/test', require('./routs/api/test'));
 // Run server
 server.listen(serverConfig.PORT, () => console.log(`Server started on port ${serverConfig.PORT}`));
 //app.listen(serverConfig.PORT, () => console.log(`Server started on port ${serverConfig.PORT}`));
+const port = 8000;
+io.listen(port);
--- server/roulette/roulette.js
+++ server/roulette/roulette.js
@@ -1,10 +1,80 @@
-exports.default = class roulette{
+ 
+
+ function Roulette (io) {
+    this.io = io;
+    this.gameState = "endGame";
+    this.users=[];
+    this.bets={};
+    //this.init()
+ }
+
+
+Roulette.prototype.endGame =function() {
+    this.gameState = "endGame";
+    result = Math.floor(Math.random() * Math.floor(37));
+    resultColor =(result==0)?"green":(result%2==0)?"red":"black";
+    for(user in this.bets){
+       for(betType in  this.bets[user]){
+           const bet = this.bets[user][betType];
+           if(betType==resultColor){
+               //user = token
+               //добавить игроку user  на счет bet монет
+           }else if(betType==result){
+                //добавить игроку user на счет bet*35
+           }
+           else {
+               //снять со счета user bet монет
+           }
+           
+       }
+    }
+    //TODO change all users money in db
+    this.io.emit("endGame",{result});
+    this.bets={};
+}
+
+Roulette.prototype.addUser =function(user) {
+    if (this.users.length<10){
+        users.push(user);
+    }
+}
+Roulette.prototype.makeBet = function(user,betType,bet) {
+    if(this.bets[user]==undefined){
+        //console.log(`set bets.${user} to {}`)
+        this.bets[user]={};
+    }
+    this.bets[user][betType]=bet;
+}
+
+Roulette.prototype.startGame = function() {
+    if(this.gameState=="endGame"){
+        console.log("start");
+        //console.log(this.gameState);
+        //console.log("starting game");
+        this.gameState = "TakingBets";
+        setTimeout(this.endGame.bind(this),45000);
+        this.io.emit("startGame");
+    }
+    else{
+        console.log(this);
+        console.log(this.gameState);
+        console.log("game is going")
+        //cb("error");
+        //error
+    }
+    console.log(this)
+}
+/* 
+ class roulette{
     constructor(io){
         this.io = io;
-        this.gameState = "TakingBets";
+        this.gameState = "endGame";
         this.users=[];
         this.bets={};
-        io.on('connection', function(socket){
+this.oninit();
+    }
+    oninit(){
+        this.io.on('connection', function(socket){
             console.log('a user connected');
             socket.on('makeBet',(msg)=>{
                 if(this.gameState=="endGame"){
@@ -12,24 +82,35 @@ exports.default = class roulette{
                 }
                 const {user,bet,betType} = msg;
                 this.makeBet(user,bet,betType);
-                io.emit("makeBet",roulette.bets);
+                this.io.emit("makeBet",roulette.bets);
             });
-            socket.on("startGame",function(msg,cb){
-                if(gameState=="endGame"){
+            socket.on("startGame", 
+            /*        
+            (msg,cb) => {
+                    if(this.gameState=="endGame"){
+                    console.log(this);
+                    console.log(this.gameState);
+                    console.log("starting game");
                     this.startGame();
                     io.emit("startGame")
                 }
                 else{
-                    cb("error");
+                    console.log(this);
+                    console.log(this.gameState);
+                    console.log("game is going")
+                    //cb("error");
                     //error
                 }
+                
             })
             socket.on('disconnect', function(){
               console.log('user disconnected');
             });
           });
     }
-     endGame(){
+
+
+    endGame(){
         this.gameState = "endGame";
         result = Math.floor(Math.random() * Math.floor(max));
         //TODO change all users money in db
@@ -45,7 +126,23 @@ exports.default = class roulette{
         this.bets[user][betType]=bet;
     }
     startGame(){
-        this.gameState = "TakingBets";
-        this.setTimeout(this.endGame,45000);
+        if(this.gameState=="endGame"){
+            console.log(this);
+            console.log(this.gameState);
+            console.log("starting game");
+            this.gameState = "TakingBets";
+            this.setTimeout(this.endGame,45000);
+            this.io.emit("startGame")
+        }
+        else{
+            console.log(this);
+            console.log(this.gameState);
+            console.log("game is going")
+            //cb("error");
+            //error
+        }
+        console.log(this)
     }
 }
+*/
+module.exports = Roulette;
